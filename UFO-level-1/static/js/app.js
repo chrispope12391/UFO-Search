351fbaaf9ef13dc95384f117ab833fa69c8ed7b0
chrispope12391 <popex107@umn.edu>
2021-04-07 22:36:13 -0500

    7

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,19 +1,30 @@
 console.log(data);
 
-var date = data.map(d => d.datetime);
-var city = data.map(d => d.city);
-var state = data.map(d => d.state);
-var country = data.map(d => d.country);
-var shape = data.map(d => d.shape);
-var duration = data.map(d => d.durationMinutes);
-var comments = data.map(d => d.comments);
+// var date = data.map(d => d.datetime);
+// var city = data.map(d => d.city);
+// var state = data.map(d => d.state);
+// var country = data.map(d => d.country);
+// var shape = data.map(d => d.shape);
+// var duration = data.map(d => d.durationMinutes);
+// var comments = data.map(d => d.comments);
 
-console.log(date);
-console.log(city);
-console.log(state);
-console.log(country);
-console.log(shape);
-console.log(duration);
-console.log(comments);
+// console.log(date);
+// console.log(city);
+// console.log(state);
+// console.log(country);
+// console.log(shape);
+// console.log(duration);
+// console.log(comments);
 
+// Get a reference to the table body
 var tbody = d3.select("tbody");
+
+// create a for loop that goes through each dictionary to find the key and value and then append the value for each key
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
