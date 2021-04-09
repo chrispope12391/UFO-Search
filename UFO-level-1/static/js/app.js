ec569707077c7c37039fc7b2be53843198908caf
chrispope12391 <popex107@umn.edu>
2021-04-08 21:09:25 -0500

    8

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,30 +1,59 @@
-console.log(data);
+// console.log(`here${data}`);
 
-// var date = data.map(d => d.datetime);
-// var city = data.map(d => d.city);
-// var state = data.map(d => d.state);
-// var country = data.map(d => d.country);
-// var shape = data.map(d => d.shape);
-// var duration = data.map(d => d.durationMinutes);
-// var comments = data.map(d => d.comments);
+// assign the data from data.js to a variable
+var ufoData = data;
 
-// console.log(date);
-// console.log(city);
-// console.log(state);
-// console.log(country);
-// console.log(shape);
-// console.log(duration);
-// console.log(comments);
+// select the button
+var button = d3.select("#filter-btn");
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+// select the form
+var form = d3.select("#form");
 
-// create a for loop that goes through each dictionary to find the key and value and then append the value for each key
-data.forEach((ufoData) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+// create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// complete the event handler function for form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3. select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(`hello${inputValue}`);
+
+    var filteredData = ufoData.filter( u => u.datetime === inputValue);
+
+    console.log(`this is where ${filteredData}`);
+
+    var tbody = d3.select("tbody");
+
+    tbody.html("");
+
+    // create a for loop that goes through each dictionary to find the key and value and then append the value for each key
+    filteredData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
     });
-  });
+
+};
+
+// // Get a reference to the table body
+// var tbody = d3.select("tbody");
+
+// // create a for loop that goes through each dictionary to find the key and value and then append the value for each key
+// data.forEach((ufoData) => {
+//     var row = tbody.append("tr");
+//     Object.entries(ufoData).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
   
